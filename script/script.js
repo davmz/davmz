7118ea40c09e8f7be963f259af5713d1db92c4ac
davmz <montanezdavid@gmail.com>
2022-05-07 12:48:52 -0500

    Disable scroll when burger menu is open

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", load);
 
 /**
- * 
+ *
  */
  function load() {
     menuButton();
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", load);
 }
 
 /**
- * 
+ *
  */
  function menuButton() {
     const menuBtn = document.querySelector(".menu-btn");
@@ -18,14 +18,19 @@ document.addEventListener("DOMContentLoaded", load);
     menuBtn.addEventListener("click", () => {
         if(!menuOpen) {
             menuBtn.classList.add("open");
+            document.body.style.overflowY = 'hidden';
             menuOpen = true;
         } else {
             menuBtn.classList.remove("open");
+            document.body.style.overflowY = 'visible';
             menuOpen = false;
         }
     });
 }
 
+/**
+ *
+ */
 function stickyNavigation() {
     window.addEventListener("scroll", () => {
         let header = document.querySelector("header");
